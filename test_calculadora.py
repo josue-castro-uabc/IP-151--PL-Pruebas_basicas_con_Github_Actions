f7d85e1aaa203c1394f9f77c319b11e82d3464c6
josue-castro-uabc <josue.castro@uabc.edu.mx>
2026-02-24 16:45:46 -0800

    Adición de prueba para método subtract de calculadora.py & ajuste de método add de calculadora.py para fallar la prueba automatizada

--- test_calculadora.py
+++ test_calculadora.py
@@ -3,3 +3,7 @@ from calculadora import Calculadora
 def test_add():
     calc = Calculadora()
     assert calc.add(2, 3) == 5
+
+def test_subtract():
+    calc = Calculadora()
+    assert calc.subtract(3, 2) == 1
