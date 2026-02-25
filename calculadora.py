c26e3f068934f0503503bc58fca4bfe0f9164b71
josue-castro-uabc <josue.castro@uabc.edu.mx>
2026-02-24 16:47:39 -0800

    Corrección del método intencionalmente erróneo add de calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,7 +1,7 @@
 class Calculadora:
     def add(self, a, b):
-        # return a + b
-        return 256  # Ajuste del método para regresar un valor arbitrario y fallar la prueba automatizada
+        return a + b
+        # return 256  # Ajuste del método para regresar un valor arbitrario y fallar la prueba automatizada
 
     def subtract(self, a, b):
         return a - b
